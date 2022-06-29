c08cc64076aa8093261d85f37ef48df4a22cc5d9
mcjovial <emmanuelelias455@gmail.com>
2022-06-29 09:05:29 +0100

    initial commit

---
+++ server.js
@@ -0,0 +1,7 @@
+const app = require('./backend/app');
+const connectDB = require('./backend/config/db');
+const port = process.env.PORT || 5000;
+
+connectDB();
+
+app.listen(port, () => console.log(`Server started on port ${port}`));
